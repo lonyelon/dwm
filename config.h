9bdbc97d79b192e4c981b00ed2d96925d529cb91
Sergio Miguéns Iglesias <sergio@lony.xyz>
2021-09-06 05:06:14 +0200

    Add XF86 keys support
    Volume, mute and brightness keys are now supported by default.

--- config.h
+++ config.h
@@ -43,6 +43,15 @@ static int attachdirection = 3;
  */
 static const int screenCount = 2;
 
+/*
+ * XF86 Keys. very useful for laptops.
+ */
+#define defSink    "46"  /* Default alsa sink */
+#define sinkVolInc "+5%" /* Percentage of volume increased for each key press */ 
+#define sinkVolDec "-5%" /* Percentage of volume decreased for each key press */
+#define defSource  "47"  /* Default alsa source */
+#define brnInc     "5"   /* Brightness increment and decrement amount */
+
 /* tagging */
 /* TODO Make tags dynamic. */
 static char *tags[] = { "1", "2", "3", "4" };
@@ -62,65 +71,81 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "dwindle.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
- 	{ "[\\]",     dwindle },
+	{ "[\\]",     dwindle },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
-    { MODKEY,                       KEY,      focusnthmon,    {.i  = TAG*screenCount/8} },        \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << (TAG - 8/screenCount*(TAG*screenCount/8))} }, \
-    { MODKEY|ShiftMask,             KEY,      tagnthmon,      {.i  = TAG*screenCount/8} },        \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << (TAG - 8/screenCount*(TAG*screenCount/8))} }, \
+	{ MODKEY,           KEY, focusnthmon, {.i  = TAG*screenCount/8} }, \
+	{ MODKEY,           KEY, view,        {.ui = 1 << (TAG - 8/screenCount*(TAG*screenCount/8))} }, \
+	{ MODKEY|ShiftMask, KEY, tagnthmon,   {.i  = TAG*screenCount/8} }, \
+	{ MODKEY|ShiftMask, KEY, tag,         {.ui = 1 << (TAG - 8/screenCount*(TAG*screenCount/8))} }, \
 
 //                                                                      COMMANDS
 ////////////////////////////////////////////////////////////////////////////////
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-l", "30", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_bg_sel, "-sf", col_fg_sel, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *quteCmd[]  = { "qutebrowser", NULL };
-static const char *neomuttCmd[] =  { "st", "-c", "float", "-g", "150x50", "-e",  "neomutt", NULL };
-static const char *mailsyncCmd[] = { "st", "-c", "float", "-g",  "80x50", "-e", "mailsync", NULL };
-static const char *newsboatCmd[] = { "st", "-c", "float", "-g", "150x50", "-e", "newsboat", NULL };
+static const char *dmenucmd[]      = { "dmenu_run",   "-l",              "30",      "-m",       dmenumon, "-fn", dmenufont,  "-nb", col_bg, "-nf", col_fg, "-sb", col_bg_sel, "-sf", col_fg_sel, NULL };
+static const char *termcmd[]       = { "st",          NULL };
+static const char *quteCmd[]       = { "qutebrowser", NULL };
+static const char *neomuttCmd[]    = { "st",          "-c",              "float",   "-g",       "150x50", "-e",  "neomutt",  NULL };
+static const char *mailsyncCmd[]   = { "st",          "-c",              "float",   "-g",       "80x50",  "-e",  "mailsync", NULL };
+static const char *newsboatCmd[]   = { "st",          "-c",              "float",   "-g",       "150x50", "-e",  "newsboat", NULL };
+static const char *muteSinkCmd[]   = { "pactl",       "set-sink-mute",   defSink,   "toggle",   NULL };
+static const char *muteSourceCmd[] = { "pactl",       "set-source-mute", defSource, "toggle",   NULL };
+static const char *volUpCmd[]      = { "pactl",       "set-sink-volume", defSink,   sinkVolInc, NULL };
+static const char *volDownCmd[]    = { "pactl",       "set-sink-volume", defSink,   sinkVolDec, NULL };
+static const char *brnUpCmd[]      = { "xbacklight",  "-inc",            brnInc,    NULL };
+static const char *brnDownCmd[]    = { "xbacklight",  "-dec",            brnInc,    NULL };
 
 /* Personal audio hack, you should probably remove it */
 static const char *pactlCmd[] = { "pactl", "set-card-profile", "alsa_card.pci-0000_08_00.1", "output:hdmi-stereo-extra4", NULL };
 
-static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      spawn,          {.v = quteCmd } },
-	{ MODKEY,                       XK_m,      spawn,          {.v = neomuttCmd } },
-	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = mailsyncCmd } },
-	{ MODKEY,                       XK_n,      spawn,          {.v = newsboatCmd } },
-	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = pactlCmd } },
+#include <X11/XF86keysym.h>
 
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+static Key keys[] = {
+	/* modifier                     key                       function        argument */
+	{ MODKEY,                       XK_d,                     spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,                spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,                     spawn,          {.v = quteCmd } },
+	{ MODKEY,                       XK_m,                     spawn,          {.v = neomuttCmd } },
+	{ MODKEY|ShiftMask,             XK_m,                     spawn,          {.v = mailsyncCmd } },
+	{ MODKEY,                       XK_n,                     spawn,          {.v = newsboatCmd } },
+	{ MODKEY|ShiftMask,             XK_a,                     spawn,          {.v = pactlCmd } },
+
+	{ MODKEY|ShiftMask,             XK_q,                     killclient,     {0} },
+
+	/* XF86 Keys */
+	{ 0,                            XF86XK_AudioMute,         spawn,          {.v = muteSinkCmd } },
+	{ 0,                            XF86XK_AudioMicMute,      spawn,          {.v = muteSourceCmd } },
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,          {.v = volUpCmd } },
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn,          {.v = volDownCmd } },
+	{ 0,                            XF86XK_MonBrightnessUp,   spawn,          {.v = brnUpCmd } },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn,          {.v = brnDownCmd } },
 
 	/* Alternating layouts */
-	{ MODKEY|ControlMask|ShiftMask, XK_d,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ControlMask|ShiftMask, XK_d,                     setlayout,      {.v = &layouts[0]} },
 
-//  { MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_Up,     focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_Down,   focusstack,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_Up,     movestack,      {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_Down,   movestack,      {.i = +1 } },
+//	{ MODKEY,                       XK_b,                     togglebar,      {0} },
+	{ MODKEY,                       XK_Up,                    focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Down,                  focusstack,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Up,                    movestack,      {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Down,                  movestack,      {.i = +1 } },
 
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_comma,                 focusmon,       {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
 
-	{ MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_Left,                  setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_Right,                 setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_space,                 togglefloating, {0} },
 
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_1,                                     0)
+	TAGKEYS(                        XK_2,                                     1)
+	TAGKEYS(                        XK_3,                                     2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
 	TAGKEYS(                        XK_6,                      5)
@@ -128,7 +153,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 
-	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      quit,                          {0} },
 };
 
 /* button definitions */
