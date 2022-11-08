93e9e5589e3fe2bdab51b73619679ede37f11ab3
Sergio Miguéns Iglesias <sergio@lony.xyz>
2022-11-08 12:20:28 +0100

    Increase bar character limit

--- dwm.c
+++ dwm.c
@@ -241,7 +241,7 @@ static int xerrorstart(Display *dpy, XErrorEvent *ee);
 
 /* variables */
 static const char broken[] = "broken";
-static char stext[1024];
+static char stext[2048];
 static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
 static int bh, blw = 0;      /* bar geometry */
