3bfbf3111dd0fa7ff334ac9b534ca57078de6c60
Sergio Miguéns Iglesias <sergio@lony.xyz>
2023-07-25 11:15:23 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  
+  buildInputs = with pkgs; [
+    xorg.libX11
+    xorg.libXft
+    xorg.libXinerama
+  ];
+}
