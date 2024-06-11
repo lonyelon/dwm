04d45aec815727343244ff5b6fb7a4ca31b11694
Sergio Miguéns Iglesias <sergio@lony.xyz>
2024-06-11 21:38:06 +0200

    Allow numbers to tag monitors again

--- config.h
+++ config.h
@@ -123,6 +123,16 @@ static Key keys[] = {
 	TAGKEYS(                        XK_c,                     7)
 	TAGKEYS(                        XK_h,                     8)
 	TAGKEYS(                        XK_l,                     9)
+	TAGKEYS(                        XK_1,                     0)
+	TAGKEYS(                        XK_2,                     1)
+	TAGKEYS(                        XK_3,                     2)
+	TAGKEYS(                        XK_4,                     3)
+	TAGKEYS(                        XK_5,                     4)
+	TAGKEYS(                        XK_6,                     5)
+	TAGKEYS(                        XK_7,                     6)
+	TAGKEYS(                        XK_8,                     7)
+	TAGKEYS(                        XK_9,                     8)
+	TAGKEYS(                        XK_0,                     9)
 };
 
 /* button definitions */
